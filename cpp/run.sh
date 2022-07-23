403b926ff9d918bc022a91b36aa912383a613a2f
Nabetani <takenori@nabetani.sakura.ne.jp>
2022-07-23 15:53:25 +0900

    go に合わせる

--- cpp/run.sh
+++ cpp/run.sh
@@ -1,14 +1,14 @@
 clang++ -Wall -O2 -std=c++17 main.cpp -o clang 
 ./clang 10
-./clang 30000
-./clang 30000
-./clang 30000
-./clang 30000
-./clang 30000
+./clang 50000
+./clang 50000
+./clang 50000
+./clang 50000
+./clang 50000
 g++-11 -Wall -O2 -std=c++17 main.cpp -o gcc11
 ./gcc11 10
-./gcc11 30000
-./gcc11 30000
-./gcc11 30000
-./gcc11 30000
-./gcc11 30000
+./gcc11 50000
+./gcc11 50000
+./gcc11 50000
+./gcc11 50000
+./gcc11 50000
